a4e008aa7e6e3392b86508cbf3f2575f613e1089
Vivek nasit <147681458+9510589079@users.noreply.github.com>
2024-11-28 16:29:54 +0000

    datatypes js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail="viveknasit167@gmail.com"
+var accountPassword="9510589079"
+ accountCity="surat"
+
+//accountId=2//
+accountEmail="vivek189@gmail.com"
+accountPassword="462122"
+accountCity="rajkot"
+
+console.log(accountId); 
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
+
