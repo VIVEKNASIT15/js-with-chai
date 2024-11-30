069c68d20cc464585417b9de2ec25bd37da4af41
Vivek nasit <147681458+9510589079@users.noreply.github.com>
2024-11-30 05:24:34 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOpration.js
@@ -0,0 +1,22 @@
+/*let score ="vivek"
+
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);*/
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+/*let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);*/
+
+let someNumber = 900
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
