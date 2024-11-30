b3ee56697157f2a8bf1b1228212c9146bc34d5c9
Vivek nasit <147681458+9510589079@users.noreply.github.com>
2024-11-30 06:13:00 +0000

    post and prefix in js

--- 01_basics/03_conversionOpration.js
+++ 01_basics/03_conversionOpration.js
@@ -16,7 +16,63 @@ console.log(valueInNumber);*/
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);*/
 
-let someNumber = 900
+/*let someNumber = 900
 let stringNumber = String(someNumber)
 console.log(stringNumber)
-console.log(typeof stringNumber);
+console.log(typeof stringNumber);*/
+
+ /******opration ********/
+
+ //let value = -151003
+ //let negValue= value
+ //console.log(negValue);
+ //console.log(typeof negValue);
+
+ /*console.log(2+2);
+ console.log(2-2);
+ console.log(2*2);
+ console.log(2**3);
+ console.log(2/3);
+ console.log(2%3);*/
+
+ /*console.log("253"+58+56);
+ console.log(58+56+56+"89");
+ console.log("8565"+"9989");
+ console.log((2+50)*8/100);*/
+
+// console.log(+"");
+
+/*let str1 = "hello"
+let str2 = "  Nasit"
+
+let str3 = str1 + str2
+ console.log(str3); */
+ 
+ //let num1, num2, num3
+
+ //num1 = num2 = num3 = 2 + 2
+  
+ /*let gameCounter =200
+ gameCounter++
+ console.log(gameCounter);*/
+
+ let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+
+
+ 
+ 
+ 
+
+ 
+ 
+ 
+
