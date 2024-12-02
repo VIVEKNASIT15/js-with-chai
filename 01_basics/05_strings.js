f2c0db64851b98c52ee2d84bb1f6cddcea746f1d
Vivek nasit <147681458+9510589079@users.noreply.github.com>
2024-12-02 06:42:27 +0000

    stringsopration

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name= "vivek"
+const repoCount=50
+ 
+//console.log(name+repoCount+"value");
+
+console.log(`hello my name is ${name} and my repo Count is ${repoCount}`);
+
+const gameName = new String ('vivekk')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.charAt(2));
+console.log(gameName.substring(0,5));
+console.log(gameName.slice(-5,4));
+
+const newStringOne="   vivek   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+ 
+const  url ="https://congenial-journey-jj5x5gjjq67vcpgvx.github.dev%20shcgds"
+console.log(url.replace('%20','-'))
+
+
+console.log(url.includes('vivek'))
+
+console.log(url.split('-'))
+
+
