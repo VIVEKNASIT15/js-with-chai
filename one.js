d5b381ae05aaa1e88f2da2eca6a767a424f90088
Vivek nasit <147681458+VIVEKNASIT15@users.noreply.github.com>
2024-12-25 14:47:12 +0530

    Add files via upload

---
+++ one.js
@@ -0,0 +1,47 @@
+// const isUserloggedIn =true
+
+// const temperature = 50
+// if (temperature==500) {
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+    
+// }
+    
+// const score =200
+
+// if (score<200) {
+//     let hello ="up"
+//     console.log(`your up ${hello}`);
+// }
+// else{
+//     let hyy ="oyy"
+//     console.log(`your up ${hyy}`);
+// }]
+
+// const balance =10000
+// if (balance<500) {
+//     console.log("less than 500");
+    
+// } else if(balance<400){
+//     console.log("less than 750");
+// }
+// else if(balance<=10000){
+//     console.log("is equal  10000");
+// }else{
+//     console.log("greater than 1100");
+    
+// }
+    
+const userLoggedIn =true
+const debitCard = true
+const loggedInFromGoogle= false
+const loggedInFromEmail = true
+ if (userLoggedIn&& debitCard) {
+    console.log("allow to buy course");
+    
+ }
+ if (loggedInFromGoogle||loggedInFromEmail) {
+console.log("user logged in");
+    
+ }
