ef1fe361f5536c90a8429bb5e6f078fa94067eb4
VIVEKNASIT15 <viveknasit167@gmail.com>
2024-12-30 15:10:21 +0530

    event listener

--- 02_basics/03_objectsbasics.js
+++ 02_basics/03_objectsbasics.js
@@ -22,18 +22,50 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-// JsUser.email = "vivek@chatgpt.com"
-// //console.log( Object.freeze(JsUser));
-// JsUser.email = "vivek@microsoft.com"
-// console.log(JsUser);
+// // JsUser.email = "vivek@chatgpt.com"
+// console.log( Object.freeze(JsUser));
+// // JsUser.email = "vivek@microsoft.com"
+// // console.log(JsUser);
 
-JsUser.greeting = function(){
-    console.log("Hello JS user");
-}
-JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
-}
+// JsUser.greeting = function(){
+//     console.log("Hello JS user");
+// }
+// JsUser.greetingTwo = function(){
+//     console.log(`Hello JS user, ${this.name}`);
+// }
 
-// console.log(JsUser.greeting());
-// console.log(JsUser.greetingTwo());
-console.log(JsUser.lastLoginDays);
+// // console.log(JsUser.greeting());
+// // console.log(JsUser.greetingTwo());
+// console.log(JsUser.lastLoginDays);
+
+const rows =5
+
+    for (let i = 1; i  <= rows; i++) {
+        let spaces = ' '.repeat(rows - i);
+        let stars = '*'.repeat(2 * i - 1);
+        console.log(spaces + stars + spaces);
+    }
+
+
+
+    function printPattern() {
+        let n = 10; 
+        for (let i = 1; i <= n; i++) {
+          let row = '';
+          for (let j = 1; j <= i; j++) {
+            row += '*';
+          }
+          console.log(row);
+        }
+      
+        for (let i = n - 1; i >= 1; i--) {
+          let row = '';
+          for (let j = 1; j <= i; j++) {
+            row += '*';
+          }
+          console.log(row);
+        }
+      }
+      
+      printPattern();
+      
