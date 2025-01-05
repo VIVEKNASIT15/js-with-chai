f2fd995dd9d486c6e2042dc369cd4f0884ae09ad
VIVEKNASIT15 <viveknasit167@gmail.com>
2025-01-05 10:50:06 +0530

    ARRAY TASK

--- 02_basics/array.js
+++ 02_basics/array.js
@@ -37,3 +37,15 @@ console.log( newArr);
 // const myn2 = myArr.splice(1, 3)
 // console.log("C ", myArr);
 // console.log(myn2);
+
+
+const intArray =[4,45,66,78,65]
+const largestValue=(arr)=>{
+    firstLargestValue=Math.max(...arr)
+    
+    index= arr.indexOf(firstLargestValue)
+    arr.splice(index,1)
+    secondLargestValue=Math.max(...arr)
+    return(secondLargestValue)
+}
+console.log(largestValue(intArray));
