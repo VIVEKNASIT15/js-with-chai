bf45820f17775851ae862691a7872987779a3324
VIVEKNASIT15 <viveknasit167@gmail.com>
2025-01-08 10:55:20 +0530

    async function

--- 08_advance/promise.js
+++ 08_advance/promise.js
@@ -42,3 +42,23 @@ promiseTwo
     console.log(error); 
 }).finally(()=> console.log("the promise is resolved"));
  
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = false
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
+});
+
+async function consumePromiseFive(){
+    try {
+        const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromiseFive()
